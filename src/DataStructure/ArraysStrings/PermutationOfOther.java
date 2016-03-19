106dc685f907a46d866e28d45c6cccf835bdf57b
arpithparikh <arpithparikh@GWLIBPUBLIC-037.ead.gwu.edu>
2016-03-19 13:44:08 -0400

    commit the changes

---
+++ src/DataStructure/ArraysStrings/PermutationOfOther.java
@@ -0,0 +1,100 @@
+//Given two strings, write a method to decide if one is a permutation of the other
+//Comparison is case sensitive and whitespace is significant.
+//is the string is having ASCII OR UNICODE UTF 8,16
+//Example: "god"=>"dog"
+
+
+package DataStructure.ArraysStrings;
+
+import java.util.HashMap;
+
+public class PermutationOfOther {
+
+	
+	public static boolean Solution(String str1,String str2)
+	{/*
+		//Character array 
+		char str11[]=str1.toCharArray();
+		char str22[]=str2.toCharArray();
+		
+		
+		//precondition
+		if(str11.length!=str22.length)
+		{
+			return false;
+		}
+		
+		HashMap<Integer,Character> hashMap=new HashMap<Integer,Character>();
+		
+		
+		for(int i=0;i<str11.length;i++)
+		{
+			if(hashMap.containsKey(str11[i]))
+			{
+				//
+				hashMap.put(hashMap.get(str11[i])+1, str11[i]);
+			}
+			else
+			{
+				hashMap.put(1,str11[i]);
+			}
+		}
+		
+		for(int i=0;i<str22.length;i++)
+		{
+			if(hashMap.containsKey(str22[i]))
+			{
+				hashMap.put(hashMap.get(str22[i])-1,str22[i]);
+				
+			}
+		}
+		
+		for(int i=0;i<str22.length;i++)
+		{
+			if(hashMap.get(str22[i])!=0)
+			
+				return false;
+		}
+		return true;*/
+		
+		
+		
+		if(str1 == null || str2 == null)
+			return false;
+		char[] str11 = str1.toCharArray();
+		char[] str22 = str2.toCharArray();
+		if(str11.length != str22.length)
+			return false;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for(int i =0;i<str11.length;i++){
+			if(map.containsKey(str11[i])){
+				map.put(str11[i],map.get(str11[i])+1);
+			}
+			else
+				map.put(str11[i],1);
+		}
+		for(int i =0;i<str22.length;i++){
+			if(map.containsKey(str22[i]))
+			{
+				map.put(str22[i], map.get(str22[i])-1);
+			}
+		}
+		for(int i =0;i<str22.length;i++){
+			if(map.get(str22[i])!=0)
+				return false;
+		}
+		return true;
+	}
+		
+	
+	
+	public static void main(String s1[])
+	{
+		String S1="abc";
+		String S2="cba";
+		
+		System.out.println(Solution(S1, S2));
+				
+	}
+
+}
--- src/DataStructure/ArraysStrings/Reverse.java
+++ src/DataStructure/ArraysStrings/Reverse.java
@@ -63,9 +63,7 @@ public class Reverse {
 		*/
 			}
 		return str;
-	
-		
-		
+
 	}
 	
 	public static void main(String[] args) {	
