99f38d4a3be52577b218df42d42707980befae17
arpithparikh <arpithparikh@GWLIBPUBLIC-103.ead.gwu.edu>
2016-03-20 11:43:38 -0400

    LinkedList sorting using Insertion sort

--- src/DataStructure/LinkedLists/InsertionSortLinkedList.java
+++ src/DataStructure/LinkedLists/InsertionSortLinkedList.java
@@ -2,23 +2,28 @@
 //Traverse the given list. 
 //a) Insert current node in sorted way in sorted or result list.
 //change the head of given linked list to head of sorted list.
-//Example:   
+//Example:   [5,3,8,1,6]
 
 
-//Using the Java collection API.
+//Using the Java collection API LinkedList using same logic of Basic Insertion Sort.
 package DataStructure.LinkedLists;
 
 public class InsertionSortLinkedList {
 	
+	// Mysort using LinkedList
 	public static java.util.LinkedList<Integer> mySort(java.util.LinkedList<Integer> linkedList)
 	{
+		//loop over
 		for(int i=1;i<linkedList.size();i++)
 		{
+			//to get-> returns the element specified position in the list.
 			int key=linkedList.get(i);
+			
 			int j=i-1;
 			
 			while(j>=0 && linkedList.get(j)>key)
 			{
+				//setting the LinkedList logic => arr[j+1]=arr[j]
 				linkedList.set(j+1,linkedList.get(j));
 				j--;
 			}
