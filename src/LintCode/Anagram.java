725606e56b3011dbe57c95eee6f35f8de220ea03
arpithparikh <arpithparikh@gmail.com>
2016-03-21 13:48:21 -0400

    Check the the number is Anagram.

--- src/LintCode/Anagram.java
+++ src/LintCode/Anagram.java
@@ -10,7 +10,7 @@ public class Anagram {
      * @param b: The second string
      * @return true or false
      */
-    public static boolean anagram(String s, String t) {
+  /*  public static boolean anagram(String s, String t) {
         // write your code here
         
         HashMap<Character,Integer> hashMap=new HashMap<Character,Integer>();
@@ -38,13 +38,74 @@ public class Anagram {
         
         return false;
     }
-	
+    
+    */
+    //changing the approach and try to work with the hash-code approach.
+    
+	//check whether is it Anagram or not!
+    public static boolean isAnagram(final String s1,final String s2)
+    {
+    	//check error 
+    	if(s1==null||s2==null||s1.length()!=s2.length())
+    		return false;
+    	
+    	//
+    	if(s1.equals(s2))
+    		return true;
+    	
+  
+    	final StringWrapper wrapper1=new StringWrapper(s1);
+    	final StringWrapper wrapper2=new StringWrapper(s2);
+    	
+    	if(wrapper1.equals(wrapper2))
+    		return true;
+    
+    	
+    	
+    	
+    	return false;
+    }
+    
+    static class StringWrapper
+    {
+    	String word;
+    	
+    	public StringWrapper(final String word)
+    	{
+    		this.word=word;
+    	}
+    	
+    	public boolean equals(final Object obj)
+    	{
+    		//both object should be the same using the hash code.
+			return hashCode()==obj.hashCode();	
+    	}
+    	
+    	public int hashCode()
+    	{
+    		//to get the character array
+    		final char[] array=word.toCharArray();
+    		
+    		//Initialize the hash-code
+    		int hashcode=0;
+    		
+    		//creating hash-code 
+    		for(final char c:array)
+    		{
+    			hashcode=hashcode+(c);
+    			
+    		}
+    		return hashcode;
+    	}
+    }
+    
 	public static void main(String args[])
 	{
-		String s1="abcd";
-		String s2="dcbe";
+		String s1="abcde";
+		String s2="edcba";
 		
-		System.out.println(anagram(s1, s2));
+		/*System.out.println(anagram(s1, s2));*/
+		System.out.println(isAnagram(s1, s2));
 		
 	}
 
