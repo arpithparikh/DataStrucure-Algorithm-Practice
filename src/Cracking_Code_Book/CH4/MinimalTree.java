2cff40ec561da319c12af6198ee4ac42fee8210d
arpithparikh@gmail.com <arpithparikh@gmail.com>
2016-03-26 17:08:45 -0400

    simple bst

---
+++ src/Cracking_Code_Book/CH4/MinimalTree.java
@@ -0,0 +1,17 @@
+package Cracking_Code_Book.CH4;
+
+
+//Given Sorted array with unique Integer Elements.
+//Create the Binary Tree with Minimal Height.
+//1.Insert into the tree the middle element of the array. 
+//2.Insert the left sub array elements.
+//3.Insert the right sub array elements.
+//4.Recurse.
+//complexity O(n logn)
+public class MinimalTree {
+	
+	
+	
+
+
+}
