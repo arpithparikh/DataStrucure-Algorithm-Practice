0b96943ba757b75f733c3166960619e1b3da1943
arpithparikh@gmail.com <arpithparikh@gmail.com>
2016-03-27 14:36:52 -0400

    kth from the last iterative approach

--- src/Cracking_Code_Book/ch2/KthfromLast.java
+++ src/Cracking_Code_Book/ch2/KthfromLast.java
@@ -10,6 +10,9 @@ package Cracking_Code_Book.ch2;
 
 public class KthfromLast {
 
+	private Node head;
+
+
 	int printKthToLast(Node head,int k)
 	{
 		//check if the head is null , the precondition of any simple problem
@@ -30,6 +33,37 @@ public class KthfromLast {
 		
 	return index;
 		
+	}
+	
+	
+	//Iterative Approach
+	
+	//1.Two pointer N1,N2
+	//2.point the N2 at Beginning 
+	//3.Moving N1 k nodes into the list
+	//4.N2 will be Length-k Nodes into the list
+	Node kthLast(Node node,int k)
+	{
+		//Two Nodes
+		//Node 1
+		Node node1=head;
+		//Node 2
+		Node node2=head;
+		
+		for(int i=0;i<k;i++)
+		{
+			if(node1==null)
+				return null;
+			
+			node1=node1.next;
+		}
+		
+		while(node1!=null)
+		{
+			node1=node1.next;
+			node2=node2.next;
+		}
+		return node2;
 	}
 	
 }
