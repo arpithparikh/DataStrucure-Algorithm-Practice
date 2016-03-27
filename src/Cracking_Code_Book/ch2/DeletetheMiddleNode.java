b7be4c7ff65c3599d8cfb3dc62a131d51cae0bc3
arpithparikh@gmail.com <arpithparikh@gmail.com>
2016-03-27 14:44:31 -0400

    Delete Middle Node

--- src/Cracking_Code_Book/ch2/DeletetheMiddleNode.java
+++ src/Cracking_Code_Book/ch2/DeletetheMiddleNode.java
@@ -2,6 +2,13 @@ package Cracking_Code_Book.ch2;
 
 public class DeletetheMiddleNode {
 	
+	//1) Take a temp node and point it to the next to the accessible node. (i.e in our case temp node pointing to node D)
+
+	//2) Copy data of temp node to accessible node. (i.e copy data of node D to node C)
+
+	//3) Now linked accessible node to next node of temp. (i.e link node C to node E).
+	
+	
 	//Deleting the node
 	boolean deleteNode(Node node)
 	{
