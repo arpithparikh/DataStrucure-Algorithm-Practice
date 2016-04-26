815d8bb9ca63acbe130d4ead38d0fb2153187f25
Arpit Parikh <arpithparikh@gmail.com>
2016-04-25 21:52:28 -0400

    Basic logic is that divide the number with 5 to figure out the number of zero trailing with the number.

---
+++ src/LintCode_Main/TrailingZero.java
@@ -0,0 +1,20 @@
+package LintCode_Main;
+//11! = 39916800, so the out should be 2
+public class TrailingZero {
+
+	 public static long trailingZeros(long n) {
+		 long sum = 0;
+	        while (n != 0) {
+	            sum += n / 5;
+	            System.out.println(sum);
+	            n /= 5;
+	        }
+	        return sum;
+	    }
+	 
+	 
+	 public static void main(String []  args){
+		 long number = 6;
+		 System.out.println(trailingZeros(number));
+	 }
+};
